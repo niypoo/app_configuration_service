d2215a55908ad27289c8fb1287efa20df006aea9
Mahmoud Nabhan <niypoo@gmail.com>
2023-08-17 17:38:43 +0300

    first commit

---
+++ lib/appInfo.config.dart
@@ -0,0 +1,91 @@
+import 'package:app_config_service/models/space.model.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+
+class AppConfigService extends GetxService {
+  static AppConfigService get to => Get.find();
+
+  // APP INFO
+  final String appName;
+  final String? appWebsite;
+  final String localStorageContainer;
+  final String appIcon;
+  final String? appIconPhotoUrl;
+
+  // Support Info
+  final String? dynamicLink;
+  final String supportEmail;
+  final String? firebaseFunctionsUrl;
+  final String? supportWhatsappNumber;
+  final String termsUrl;
+  final String privacyUrl;
+  final String? tutorialLink;
+  final String? invitationImage;
+
+  // Bundle Info
+  final String? googlePlayIdentifier;
+  final String? bundleId;
+  final int? appStoreIdentifier;
+
+  // Animation
+  final Duration duration;
+  final Cubic curve;
+
+  // Spacing
+  final Space? space;
+
+  // radius
+  final double radius;
+
+  // Authentication Provider
+  final bool authenticationWithGoogle;
+  final bool authenticationWithApple;
+  final bool authenticationWithFacebook;
+  final bool authenticationWithTwitter;
+  final bool authenticationWithLinkedin;
+  final bool authenticationWithGithub;
+  final bool authenticationAsAnonymously;
+
+  AppConfigService({
+    this.appName = 'Un-named App',
+    this.appIcon = 'assets/icon/icon.jpeg',
+    this.appWebsite,
+    this.localStorageContainer = 'app.storage',
+    this.appIconPhotoUrl,
+    // Support Info
+    this.dynamicLink,
+    required this.supportEmail,
+    this.firebaseFunctionsUrl,
+    this.supportWhatsappNumber,
+    required this.termsUrl,
+    required this.privacyUrl,
+    this.tutorialLink,
+    this.invitationImage,
+
+    // Bundle Info
+    this.googlePlayIdentifier,
+    this.bundleId,
+    this.appStoreIdentifier,
+
+    // Animation
+    this.curve = Curves.easeInOut,
+    this.duration = const Duration(milliseconds: 300),
+    this.radius = 22,
+
+    // Space
+    this.space,
+
+    // Authentication Provider
+    this.authenticationWithGoogle = true,
+    this.authenticationWithApple = true,
+    this.authenticationWithFacebook = false,
+    this.authenticationWithTwitter = false,
+    this.authenticationWithLinkedin = false,
+    this.authenticationWithGithub = false,
+    this.authenticationAsAnonymously = true,
+  });
+
+  Future<AppConfigService> init() async {
+    return this;
+  }
+}
---
+++ lib/models/space.model.dart
@@ -0,0 +1,19 @@
+class Space {
+  final double xxs;
+  final double xs;
+  final double s;
+  final double m;
+  final double l;
+  final double xl;
+  final double xxl;
+
+  Space({
+    this.xxs = 2.5,
+    this.xs = 5,
+    this.s = 10,
+    this.m = 16,
+    this.l = 20,
+    this.xl = 40,
+    this.xxl = 60,
+  });
+}
